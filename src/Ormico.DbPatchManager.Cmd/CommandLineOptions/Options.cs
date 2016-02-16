9c90bce1b60a03f78675d27229ccc13457eb3ff9
Zack Moore <z@ormi.co>
2016-02-16 11:30:09 -0500

    add json.net; add nuget packages.config; add resolver for id json references

---
+++ src/Ormico.DbPatchManager.Cmd/CommandLineOptions/Options.cs
@@ -0,0 +1,56 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ormico.DbPatchManager.Cmd.CommandLineOptions
+{
+    class Options
+    {
+        public Options()
+        {
+        }
+
+        [VerbOption("init", HelpText = "")]
+        public InitOptions AddVerb { get; set; }
+
+        [VerbOption("addpatch", HelpText = "Create a new Patch folder and add it to odpm.json")]
+        public AddPatchOptions AddPatchVerb { get; set; }
+
+        [VerbOption("build", HelpText = "")]
+        public BuildOptions TagVerb { get; set; }
+    }
+
+    class InitOptions
+    {
+        /// <summary>
+        /// sql server, mysql, sqlite, etc
+        /// </summary>
+        [Option("dbtype", Required = true, HelpText = "sql server, mysql, sqlite, etc")]
+        public string DbType { get; set; }
+
+        /// <summary>
+        /// folder that will contain patch folders
+        /// </summary>
+        [Option("patchFolder", DefaultValue = "Patches", Required = true, HelpText = "folder that will contain patch folders")]
+        public string PatchFolder { get; set; }
+
+        /// <summary>
+        /// sprocs, functions, views
+        /// </summary>
+        [Option("codeFolder", DefaultValue = "Code", Required = true, HelpText = "sprocs, functions, views")]
+        public string CodeFolder { get; set; }
+    }
+
+    class AddPatchOptions
+    {
+        [Option("name", HelpText = "patch name")]
+        public bool Name { get; set; }
+    }
+
+    class BuildOptions
+    {
+    }
+}
