2b834a0f126590e0e606799be5c346134a9dcada
Zack Moore <ormico@users.noreply.github.com>
2016-03-06 14:20:22 -0500

    replace calls to JsonConvert.SerializeObject() and DeserializeObject() with manual serialize/deserialize to preserve references the way I want

--- src/Ormico.DbPatchManager/BuildConfigurationWriter.cs
+++ src/Ormico.DbPatchManager/BuildConfigurationWriter.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO.Abstractions;
@@ -37,19 +38,66 @@ namespace Ormico.DbPatchManager
             DatabaseBuildConfiguration rc = null;
             if(_io.File.Exists(_filePath))
             {
-                rc = JsonConvert.DeserializeObject<DatabaseBuildConfiguration>(_io.File.ReadAllText(_filePath));
+                //rc = JsonConvert.DeserializeObject<DatabaseBuildConfiguration>(_io.File.ReadAllText(_filePath), _jsonSettings);
+                var o = (Newtonsoft.Json.Linq.JObject)Newtonsoft.Json.Linq.JToken.Parse(_io.File.ReadAllText(_filePath));
+                rc = new DatabaseBuildConfiguration();
+                rc.CodeFolder = (string)o["CodeFolder"];
+                rc.ConnectionString = (string)o["ConnectionString"];
+                rc.DatabaseType = (string)o["DatabaseType"];
+                rc.PatchFolder = (string)o["PatchFolder"];
+                
+                // populate patch list
+                var patches = (from p in o["patches"]
+                             select new Patch()
+                             {
+                                 Id = (string)p["id"]
+                             }).ToList();
+                // populate DependsOn
+                foreach(var p in patches)
+                {
+                    var cur = from x in o["patches"]
+                                from d in x["dependsOn"]
+                                from a in patches
+                                where (string)x["id"] == p.Id &&
+                                    a.Id == (string)d
+                                select a;
+                    p.DependsOn = cur.ToList();
+                    //todo: double check this query
+                    var children = from x in o["patches"]
+                                   from d in x["dependsOn"]
+                                   from a in patches
+                                   where (string)d == p.Id && (string)x["id"] == a.Id
+                                   select a;
+                    p.Children = children.ToList();
+                }
+                rc.patches = patches.ToList();
+            }
+            else
+            {
+                throw new ApplicationException("Configuration file does not exist. Call init first.");
             }
             return rc;
         }
 
         public void Write(DatabaseBuildConfiguration buildConfiguration)
         {
-            JsonSerializerSettings settings = new JsonSerializerSettings()
+            //string data = JsonConvert.SerializeObject(buildConfiguration, Formatting.Indented, _jsonSettings);
+            JObject data = JObject.FromObject(new
             {
-                ReferenceResolverProvider = () => new PatchIdReferenceResolver()
-            };
-            string data = JsonConvert.SerializeObject(buildConfiguration, Formatting.Indented, settings);
-            _io.File.WriteAllText(_filePath, data);
+                DatabaseType = buildConfiguration.DatabaseType,
+                ConnectionString = buildConfiguration.ConnectionString,
+                CodeFolder = buildConfiguration.CodeFolder,
+                PatchFolder = buildConfiguration.PatchFolder,
+                patches = from p in buildConfiguration.patches
+                          select new
+                          {
+                              id = p.Id,
+                              dependsOn = p.DependsOn != null?(from d in p.DependsOn
+                                          select d.Id):null
+                          }
+            });
+
+            _io.File.WriteAllText(_filePath, data.ToString());
         }
     }
 }
