ed58eba82440fdb0df94b448b8e607870073b7b7
Zack Moore <ormico@users.noreply.github.com>
2016-03-06 14:14:17 -0500

    add GetFirstPatch(); GetOpenPatches() add where to patch search;

--- src/Ormico.DbPatchManager/DatabaseBuildConfiguration.cs
+++ src/Ormico.DbPatchManager/DatabaseBuildConfiguration.cs
@@ -25,10 +25,39 @@ namespace Ormico.DbPatchManager
 
         public List<Patch> GetOpenPatches()
         {
+            var depends = (from p in patches
+                          from d in p.DependsOn
+                          select d.Id).Distinct();
+
             var rc = from p in patches
-                             where p.DependsOn == null || p.DependsOn.Count() > 0
+                             where !depends.Contains(p.Id)
                              select p;
             return rc.ToList();
+        }
+
+        public Patch GetFirstPatch()
+        {
+            Patch rc = null;
+            var firstQuery = from f in patches
+                        where f.DependsOn == null || f.DependsOn.Count() <= 0
+                        select f;
+            var first = firstQuery.ToList();
+            if(first.Count() == 1)
+            {
+                rc = first.FirstOrDefault();
+            }
+            else if (first.Count() <= 0)
+            {
+                //todo: throw an exception or return null?
+                //throw new ApplicationException("No starting Patch found. There should be one patch with no dependencies.");
+                rc = null;
+            }
+            else
+            {
+                throw new ApplicationException("More than one starting Patch found. There should only be one Patch with no dependencies.");
+            }
+
+            return rc;
         }
     }
 }
