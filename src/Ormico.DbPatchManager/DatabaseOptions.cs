597847f3e5758a02eb5a566f57872fdb8b6405fb
Zack Moore <z@ormi.co>
2016-03-13 22:10:15 -0400

    add DatabaseOptions.cs and OdbcDatabase; remove PatchIdReferenceResolver.cs

---
+++ src/Ormico.DbPatchManager/DatabaseOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ormico.DbPatchManager
+{
+    public class DatabaseOptions
+    {
+        public DatabaseOptions()
+        {
+            AdditionalOptions = new Dictionary<string, string>();
+        }
+
+        public string ConnectionString { get; set; }
+        public string GetInstalledPatchesSql { get; set; }
+        public string AddInstalledPatchSql { get; set; }
+        public string InitPatchTableSql { get; set; }
+
+
+        public Dictionary<string, string> AdditionalOptions { get; protected set; }
+    }
+}
---
+++ src/Ormico.DbPatchManager/OdbcDatabase.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ormico.DbPatchManager
+{
+    public class OdbcDatabase : IDatabase
+    {
+        public void Connect(DatabaseOptions Options)
+        {
+            _con = new OdbcConnection(Options.ConnectionString);
+        }
+
+        OdbcConnection _con;
+
+        public void Dispose()
+        {
+            if(_con != null)
+            {
+                _con.Dispose();
+            }
+        }
+
+        public void ExecuteDDL(string commandText)
+        {
+            _con.Execute(commandText);
+        }
+
+        public List<InstalledPatchInfo> GetInstalledPatches()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInstalledPatch(string patchId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
