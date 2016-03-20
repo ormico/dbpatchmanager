df95cbf68cbaef5deb162309433f33992e5db9d3
Zack Moore <ormico@users.noreply.github.com>
2016-03-20 12:33:30 -0400

    add sql script to drop stored procedures, views, functions

---
+++ src/Ormico.DbPatchManager.SqlServer/SqlScripts/temp.sql
@@ -0,0 +1,12 @@
+﻿declare @schemas xml, @defaultschemas xml = '<schemas><s v="dbo"/></schemas>'
+set @schemas = @x
+--select @defaultschemas
+
+--<schemas>
+--	<s v="dbo"/>
+--	<s v="api"/>
+--</schemas>
+select
+	[schemas].s.value('@v', 'VARCHAR(MAX)') v
+from @schemas.nodes('/schemas/s') as [schemas](s)
+
