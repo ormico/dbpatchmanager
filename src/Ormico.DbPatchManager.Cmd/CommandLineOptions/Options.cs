3e022ab078d928bf125ca89e84eb174eca0f905c
Zack Moore <z@ormi.co>
2017-07-10 03:05:09 -0400

    fix error and remove ambiguity by renaming command line options like InitOptions to InitCmdLineOptions to not conflict w/other InitOptions object; switched to beta version of CommandLineParser and fixed command line parsing errors

--- src/Ormico.DbPatchManager.Cmd/CommandLineOptions/Options.cs
+++ src/Ormico.DbPatchManager.Cmd/CommandLineOptions/Options.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,24 +8,33 @@ using System.Threading.Tasks;
 
 namespace Ormico.DbPatchManager.Cmd.CommandLineOptions
 {
-    class Options
+    //class Options
+    //{
+    //    public Options()
+    //    {
+    //        InitVerb = new InitOptions();
+    //    }
+
+    //    public InitOptions InitVerb { get; set; }
+
+    //    public AddPatchOptions AddPatchVerb { get; set; }
+
+    //    public BuildOptions BuildVerb { get; set; }
+
+    //    [HelpVerbOption]
+    //    public string GetUsage(string verb)
+    //    {
+    //        return HelpText.AutoBuild(this, verb);
+    //    }
+    //}
+
+    [Verb("init", HelpText = "init")]
+    class InitCmdLineOptions
     {
-        public Options()
+        public InitCmdLineOptions()
         {
         }
 
-        [VerbOption("init", HelpText = "init")]
-        public InitOptions InitVerb { get; set; }
-
-        [VerbOption("addpatch", HelpText = "Create a new Patch folder and add it to odpm.json")]
-        public AddPatchOptions AddPatchVerb { get; set; }
-
-        [VerbOption("build", HelpText = "build")]
-        public BuildOptions BuildVerb { get; set; }
-    }
-
-    class InitOptions
-    {
         /// <summary>
         /// sql server, mysql, sqlite, etc
         /// </summary>
@@ -34,23 +44,32 @@ namespace Ormico.DbPatchManager.Cmd.CommandLineOptions
         /// <summary>
         /// folder that will contain patch folders
         /// </summary>
-        [Option("patchFolder", DefaultValue = "Patches", Required = true, HelpText = "folder that will contain patch folders")]
+        [Option("patchFolder", Default = "Patches", HelpText = "folder that will contain patch folders")]
         public string PatchFolder { get; set; }
 
         /// <summary>
         /// sprocs, functions, views
         /// </summary>
-        [Option("codeFolder", DefaultValue = "Code", Required = true, HelpText = "sprocs, functions, views")]
+        [Option("codeFolder", Default = "Code", HelpText = "sprocs, functions, views")]
         public string CodeFolder { get; set; }
     }
 
-    class AddPatchOptions
+    [Verb("addpatch", HelpText = "Create a new Patch folder and add it to odpm.json")]
+    class AddPatchCmdLineOptions
     {
+        public AddPatchCmdLineOptions()
+        {
+        }
+
         [Option("name", HelpText = "patch name")]
         public string Name { get; set; }
     }
 
-    class BuildOptions
+    [Verb("build", HelpText = "build")]
+    class BuildCmdLineOptions
     {
+        public BuildCmdLineOptions()
+        {
+        }
     }
 }
