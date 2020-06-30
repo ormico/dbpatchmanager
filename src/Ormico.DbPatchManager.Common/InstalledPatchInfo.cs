812a5925644d4937dafaac74036ad5f1571c101d
Zack Moore <1731364+ormico@users.noreply.github.com>
2020-06-29 23:38:52 -0400

    split out common lib; update to dotnet core 3.1

--- src/Ormico.DbPatchManager.Common/InstalledPatchInfo.cs
+++ src/Ormico.DbPatchManager.Common/InstalledPatchInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ormico.DbPatchManager.Logic
+namespace Ormico.DbPatchManager.Common
 {
     public class InstalledPatchInfo
     {
--- src/Ormico.DbPatchManager.Logic/TestDatabase.cs
+++ src/Ormico.DbPatchManager.Logic/TestDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Ormico.DbPatchManager.Common;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
