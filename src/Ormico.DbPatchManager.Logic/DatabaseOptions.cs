7ebbd6e06ecb6c024b351700b90810be31403264
Zack Moore <1731364+ormico@users.noreply.github.com>
2021-03-04 02:02:15 -0500

    update nuget and switch to .net 5

---
+++ src/Ormico.DbPatchManager.Logic/DatabaseOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ormico.DbPatchManager.Logic
+{
+    public struct DatabaseOptions
+    {
+        //public DatabaseOptions()
+        //{
+        //    AdditionalOptions = new Dictionary<string, string>();
+        //}
+
+        public string ConnectionString { get; set; }
+        public string GetInstalledPatchesSql { get; set; }
+        public string AddInstalledPatchSql { get; set; }
+        public string InitPatchTableSql { get; set; }
+
+
+        public Dictionary<string, string> AdditionalOptions { get; set; }
+    }
+}
